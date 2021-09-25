55541069abf711bb4d5b7745816cb6bc5906d68a
Panagiotis Skarlas <pskarlas@users.noreply.github.com>
2021-09-25 12:12:08 +0300

    - clean up - proper optionsn for select

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,58 +1,12 @@
-class RecipesController < ApplicationController
-  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+# frozen_string_literal: true
 
-  # GET /recipes
-  def index
-    @recipes = Recipe.all
-  end
+class RecipesController < ApplicationController
 
   # GET /recipes/1
   def show
+    recipe = Recipe.find_by(id: params[:id], slug: params[:slug])
+    @recipe = RecipeDecorator.new recipe, view_context
   end
 
-  # GET /recipes/new
-  def new
-    @recipe = Recipe.new
-  end
 
-  # GET /recipes/1/edit
-  def edit
-  end
-
-  # POST /recipes
-  def create
-    @recipe = Recipe.new(recipe_params)
-
-    if @recipe.save
-      redirect_to @recipe, notice: 'Recipe was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /recipes/1
-  def update
-    if @recipe.update(recipe_params)
-      redirect_to @recipe, notice: 'Recipe was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /recipes/1
-  def destroy
-    @recipe.destroy
-    redirect_to recipes_url, notice: 'Recipe was successfully destroyed.'
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_recipe
-      @recipe = RecipeDecorator.new Recipe.find_by_slug(params[:slug]), view_context
-    end
-
-    # Only allow a list of trusted parameters through.
-    def recipe_params
-      params.fetch(:recipe, {})
-    end
 end
