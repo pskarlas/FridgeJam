6ce09bba50bd10abee2e2fff2238081ff732f871
Panagiotis Skarlas <pskarlas@users.noreply.github.com>
2021-09-28 13:00:09 +0300

    refactored seed.rb
    removed text editors git comment
    removed redundant methods namespace

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,9 @@
 include RecipeImportHelper
 
-IO.foreach('./lib/assets/recipes/recipes.json')
-  .first(2000)
+IO.foreach('./lib/assets/recipes/recipes.json').first(100)
   .map {|raw_line| JSON.parse(raw_line)}
   .each do |recipe_hash|
-    recipe = RecipeImportHelper::find_or_create_recipe(recipe_hash)
-    RecipeImportHelper::find_or_create_ingredient(recipe, recipe_hash['ingredients'])
-    RecipeImportHelper::find_or_create_tag(recipe, recipe_hash['tags'])
+    recipe = find_or_create_recipe(recipe_hash)
+    find_or_create_ingredient(recipe, recipe_hash['ingredients'])
+    find_or_create_tag(recipe, recipe_hash['tags'])
   end
-
-
-
-
